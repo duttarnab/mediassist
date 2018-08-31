250c11633f9fded168eefe7afd4202124cd8980b
Arnab Dutta <arnab.bdutta@gmail.com>
2018-08-31 12:46:10 +0530

    first commit

--- api/controllers/DiseasesController.js
+++ api/controllers/DiseasesController.js
@@ -6,16 +6,44 @@ module.exports = {
             if(req.body.queryResult.parameters.disease == 'feaver'){
                 return res.ok (
                     {
-                    
-                    "fulfillmentMessages": [
-                      {
-                        "text": {
-                            "text": [
-                                "Hello I m Responding to intent"
-                            ]
+                        "fulfillmentText": "This is a text response",
+                        "fulfillmentMessages": [
+                            {
+                            "card": {
+                                "title": "card title",
+                                "subtitle": "card text",
+                                "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+                                "buttons": [
+                                {
+                                    "text": "button text",
+                                    "postback": "https://assistant.google.com/"
+                                }
+                                ]
+                            }
+                            }
+                        ],
+                        "source": "example.com",
+                        "payload": {
+                            "google": {
+                            "expectUserResponse": true,
+                            "richResponse": {
+                                "items": [
+                                {
+                                    "simpleResponse": {
+                                    "textToSpeech": "this is a simple response"
+                                    }
+                                }
+                                ]
+                            }
+                            },
+                            "facebook": {
+                            "text": "Hello, Facebook!"
+                            },
+                            "slack": {
+                            "text": "This is a text response for Slack."
+                            }
                         }
                     }
-                    ]}     
                     
                     /*{
                     "fulfillmentText": "This is a text response.\n\n Annother line",
