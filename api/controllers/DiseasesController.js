a0e610a38c117c08e2d25cb8bd3bd3b7f30da163
Arnab Dutta <arnab.bdutta@gmail.com>
2018-08-31 12:16:56 +0530

    first commit

--- api/controllers/DiseasesController.js
+++ api/controllers/DiseasesController.js
@@ -6,39 +6,22 @@ module.exports = {
             if(req.body.queryResult.parameters.disease == 'feaver'){
                 return res.ok (
                     {
-                        "payload": {
-                          "google": {
-                            "expectUserResponse": true,
-                            "richResponse": {
-                              "items": [
-                                {
-                                  "simpleResponse": {
-                                    "textToSpeech": "This is a Basic Card:"
-                                  }
-                                },
-                                {
-                                  "basicCard": {
-                                    "title": "Card Title",
-                                    "image": {
-                                      "url": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
-                                      "accessibilityText": "Google Logo"
-                                    },
-                                    "buttons": [
-                                      {
-                                        "title": "Button Title",
-                                        "openUrlAction": {
-                                          "url": "https://www.google.com"
-                                        }
-                                      }
-                                    ],
-                                    "imageDisplayOptions": "WHITE"
-                                  }
-                                }
-                              ]
+                    
+                    "fulfillmentMessages": [
+                      {
+                        "card": {
+                          "title": "card title",
+                          "subtitle": "card text",
+                          "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+                          "buttons": [
+                            {
+                              "text": "button text",
+                              "postback": "https://assistant.google.com/"
                             }
-                          }
+                          ]
                         }
                       }
+                    ]}     
                     
                     /*{
                     "fulfillmentText": "This is a text response.\n\n Annother line",
