271e230a184db92cafaf9db4fab9b36eefdf0f0e
Arnab Dutta <arnab.bdutta@gmail.com>
2018-08-31 12:34:18 +0530

    first commit

--- api/controllers/DiseasesController.js
+++ api/controllers/DiseasesController.js
@@ -9,18 +9,12 @@ module.exports = {
                     
                     "fulfillmentMessages": [
                       {
-                        "card": {
-                          "title": "card title",
-                          "subtitle": "card text",
-                          "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-                          "buttons": [
-                            {
-                              "text": "button text",
-                              "postback": "https://assistant.google.com/"
-                            }
-                          ]
+                        "text": {
+                            "text": [
+                                "Hello I m Responding to intent"
+                            ]
                         }
-                      }
+                    }
                     ]}     
                     
                     /*{
